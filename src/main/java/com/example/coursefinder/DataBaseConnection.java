639919a1b7c5b7226fba8b70adb7c84496a7c470
Astan Serikov <serikovastik@gmail.com>
2021-12-30 20:16:51 +0600

    everything is works

--- src/main/java/com/example/coursefinder/DataBaseConnection.java
+++ src/main/java/com/example/coursefinder/DataBaseConnection.java
@@ -4,14 +4,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DataBaseConnection {
-  public Connection databaseLink;
-
-  public Connection getDBConnection(){
+  public static Connection getDBConnection(){
       String databaseName = "postgres";
-      String databaseUser = "newuser";
-      String databasePassword = "newuser1234";
-      String url = "jdbc:postgresql://localhost:5433/"+databaseName;
-
+      String databaseUser = "postgres";
+      String databasePassword = "2283";
+      String url = "jdbc:postgresql://localhost:5432/"+databaseName;
+      Connection databaseLink = null;
       try {
           Class.forName("org.postgresql.Driver");
           databaseLink = DriverManager.getConnection(url,databaseUser,databasePassword);
